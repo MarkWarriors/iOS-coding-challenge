67f743a111c7ef1e48b8fbcf08e7b6d911fe0453
Marco Guerrieri <marcoguerrieri@gmail.com>
2018-11-28 10:51:36 +0100

    [IMP] Created the project

---
+++ GAWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GAWeather
+//
+//  Created by Marco Guerrieri on 28/11/18.
+//  Copyright © 2018 Marco Guerrieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
