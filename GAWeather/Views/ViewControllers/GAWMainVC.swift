6e6b0e45a94889270a055562c4fcca5c452c222c
Marco Guerrieri <marcoguerrieri@gmail.com>
2018-11-28 11:10:03 +0100

    [IMP] Created MainVC + ViewModel

---
+++ GAWeather/Views/ViewControllers/GAWMainVC.swift
@@ -0,0 +1,22 @@
+//
+//  GAWMainVC.swift
+//  GAWeather
+//
+//  Created by Marco Guerrieri on 28/11/18.
+//  Copyright © 2018 Marco Guerrieri. All rights reserved.
+//
+
+import UIKit
+
+class GAWMainVC: GAWViewController, ViewModelBased {
+    typealias ViewModel = GAWMainViewModel
+    var viewModel: GAWMainViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func bindViewModel() {
+        
+    }
+}
